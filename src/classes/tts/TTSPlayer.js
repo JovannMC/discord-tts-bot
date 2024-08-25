5dabc374caef01cd2faa6640683964ee5c7cf448
JovannMC <jovannmc@femboyfurry.net>
2024-08-25 10:54:02 +0300

    Fix linting errors

--- src/classes/tts/TTSPlayer.js
+++ src/classes/tts/TTSPlayer.js
@@ -43,13 +43,85 @@ class TTSPlayer {
     });
   }
 
+  async formatMessage(message, userAliases, guildAliases, extras) {
+    const originalSentence = message;
+    let finalSentence = message;
+  
+    // User aliases
+    for (const [key, value] of Object.entries(userAliases)) {
+      const regex = new RegExp(`\\b${key}\\b`, 'gi');
+      if (!regex.test(finalSentence)) {
+        continue;
+      }
+      
+      finalSentence = finalSentence.replace(regex, value);
+      logger.info(`Replaced "${key}" with "${value}" in the sentence "${originalSentence}". Found in user aliases.`);
+    }
+    
+    // Guild aliases
+    for (const [key, value] of Object.entries(guildAliases)) {
+      const regex = new RegExp(`\b${key}\\b`, 'gi');
+      if (!regex.test(finalSentence)) {
+        continue;
+      }
+      
+      finalSentence = finalSentence.replace(regex, value);
+      logger.info(`Replaced "${key}" with "${value}" in the sentence "${originalSentence}". Found in guild aliases.`);
+    }
+  
+    // If the sentence contains a link
+    if (/(https?:\/\/[^\s]+)/g.test(finalSentence)) {
+      if (/^(https?:\/\/[^\s]+)$/.test(finalSentence)) {
+        finalSentence = finalSentence.replace(/(https?:\/\/[^\s]+)/g, 'A link');
+      } else {
+        finalSentence = finalSentence.replace(/(https?:\/\/[^\s]+)/g, '. A link.');
+      }
+      logger.info(`Replaced link in the sentence "${originalSentence}".`);
+    }
+    
+    // If the sentence contains a multiline or single line codeblock
+    if (/(```[\s\S]+?```|`[^`]+`)/g.test(finalSentence)) {
+      if (/^(```[\s\S]+?```|`[^`]+`)$/.test(finalSentence)) {
+        finalSentence = finalSentence.replace(/(```[\s\S]+?```|`[^`]+`)/g, 'codeblock.');
+      } else {
+        finalSentence = finalSentence.replace(/(```[\s\S]+?```|`[^`]+`)/g, '. codeblock.');
+      }
+      logger.info(`Replaced codeblock in the sentence "${originalSentence}".`);
+    }
+  
+    // If the sentence has 3 or more punctuation marks in a row
+    if (/([^\w\s])\1{2,}/g.test(finalSentence)) {
+      finalSentence = finalSentence.replace(/([^\w\s])\1{2,}/g, '$1');
+      logger.info(`Replaced 3 or more repeating punctuation marks in the sentence "${originalSentence}".`);
+    }
+    
+    // If the sentence has 3 or more repeating characters in a row
+    if (/(\w)\1{2,}/g.test(finalSentence)) {
+      finalSentence = finalSentence.replace(/(\w)\1{2,}/g, 'Line of $1\'s, ');
+      logger.info(`Replaced 3 or more repeating characters in the sentence "${originalSentence}".`);
+    }
+  
+    // If message contains images and/or files
+    if (extras?.hasImage || extras?.hasFile) {
+      const attachmentMessage = `${[
+        extras.hasImage ? 'Images' : '',
+        extras.hasFile ? 'Files' : ''
+      ].filter(Boolean).join(' and ')} attached`;
+    
+      finalSentence = finalSentence ? `${finalSentence}. ${attachmentMessage}.` : attachmentMessage;
+      logger.info(`${attachmentMessage} with message "${originalSentence}".`);
+    }
+  
+    return finalSentence;
+  }
+
   async say(sentence, member, providerName, extras = {}) {
     const userSettings = await this.client.ttsSettings.get(member);
     const guildSettings = await this.client.ttsSettings.get(this.guild);
     const userAliases = userSettings.aliases || {};
     const guildAliases = guildSettings.aliases || {};
 
-    const finalSentence = await formatMessage(sentence, userAliases, guildAliases, extras);
+    const finalSentence = await this.formatMessage(sentence, userAliases, guildAliases, extras);
     logger.info(`Final sentence passed to TTS provider: "${finalSentence}".`);
   
     const provider = this.providerManager.getProvider(providerName);
@@ -144,78 +216,6 @@ class TTSPlayer {
       this.disconnectScheduler.stop();
     }
   }
-}
-
-async function formatMessage(message, userAliases, guildAliases, extras) {
-  const originalSentence = message;
-  let finalSentence = message;
-
-  // User aliases
-  for (const [key, value] of Object.entries(userAliases)) {
-    const regex = new RegExp(`\\b${key}\\b`, 'gi');
-    if (!regex.test(finalSentence)) {
-      continue;
-    }
-    
-    finalSentence = finalSentence.replace(regex, value);
-    logger.info(`Replaced "${key}" with "${value}" in the sentence "${originalSentence}". Found in user aliases.`);
-  }
-  
-  // Guild aliases
-  for (const [key, value] of Object.entries(guildAliases)) {
-    const regex = new RegExp(`\b${key}\\b`, 'gi');
-    if (!regex.test(finalSentence)) {
-      continue;
-    }
-    
-    finalSentence = finalSentence.replace(regex, value);
-    logger.info(`Replaced "${key}" with "${value}" in the sentence "${originalSentence}". Found in guild aliases.`);
-  }
-
-  // If the sentence contains a link
-  if (/(https?:\/\/[^\s]+)/g.test(finalSentence)) {
-    if (/^(https?:\/\/[^\s]+)$/.test(finalSentence)) {
-      finalSentence = finalSentence.replace(/(https?:\/\/[^\s]+)/g, 'A link');
-    } else {
-      finalSentence = finalSentence.replace(/(https?:\/\/[^\s]+)/g, '. A link.');
-    }
-    logger.info(`Replaced link in the sentence "${originalSentence}".`);
-  }
-  
-  // If the sentence contains a multiline or single line codeblock
-  if (/(```[\s\S]+?```|`[^`]+`)/g.test(finalSentence)) {
-    if (/^(```[\s\S]+?```|`[^`]+`)$/.test(finalSentence)) {
-      finalSentence = finalSentence.replace(/(```[\s\S]+?```|`[^`]+`)/g, 'codeblock.');
-    } else {
-      finalSentence = finalSentence.replace(/(```[\s\S]+?```|`[^`]+`)/g, '. codeblock.');
-    }
-    logger.info(`Replaced codeblock in the sentence "${originalSentence}".`);
-  }
-
-  // If the sentence has 3 or more punctuation marks in a row
-  if (/([^\w\s])\1{2,}/g.test(finalSentence)) {
-    finalSentence = finalSentence.replace(/([^\w\s])\1{2,}/g, '$1');
-    logger.info(`Replaced 3 or more repeating punctuation marks in the sentence "${originalSentence}".`);
-  }
-  
-  // If the sentence has 3 or more repeating characters in a row
-  if (/(\w)\1{2,}/g.test(finalSentence)) {
-    finalSentence = finalSentence.replace(/(\w)\1{2,}/g, 'Line of $1\'s, ');
-    logger.info(`Replaced 3 or more repeating characters in the sentence "${originalSentence}".`);
-  }
-
-  // If message contains images and/or files
-  if (extras?.hasImage || extras?.hasFile) {
-    const attachmentMessage = `${[
-      extras.hasImage ? 'Images' : '',
-      extras.hasFile ? 'Files' : ''
-    ].filter(Boolean).join(' and ')} attached`;
-  
-    finalSentence = finalSentence ? `${finalSentence}. ${attachmentMessage}.` : attachmentMessage;
-    logger.info(`${attachmentMessage} with message "${originalSentence}".`);
-  }
-
-  return finalSentence;
 }
 
 module.exports = TTSPlayer;
